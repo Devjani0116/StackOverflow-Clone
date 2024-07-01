7e3c804e198b869c5444c4f52bf575335495b64f
Devjani0116 <146856550+Devjani0116@users.noreply.github.com>
2024-07-01 10:22:40 +0530

    Update users.js

--- server/routes/users.js
+++ server/routes/users.js
@@ -1,13 +1,10 @@
 import express from 'express';
-import { forgotPassword, login, resetPassword, signup } from '../controllers/auth.js';
+import { login,  signup } from '../controllers/auth.js';
 import { getAllUsers, updateProfile } from '../controllers/users.js';
 import auth from '../middleware/auth.js';
  const router =express.Router();
 router.post('/signup',signup);
 router.post('/login',login);
-router.post('/forgot-password', forgotPassword);
-router.post('/reset-password/:token', resetPassword);
-
 router.get('/getAllUsers',getAllUsers)
 router.patch('/update/:id',auth,updateProfile)
 export default router;
